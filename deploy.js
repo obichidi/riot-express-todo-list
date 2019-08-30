7c91c69f7d883df6a53ffa40f85b3c40c767014e
Obi Chidi <innocia2009@gmail.com>
2019-08-30 19:47:15 -0400

    adding .travis.yml and deploy.sh files

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '172.31.40.52',
+      host: '34.207.250.207',
       username: 'ubuntu',
       privateKey: 'Woz-Key.pem'
     })
